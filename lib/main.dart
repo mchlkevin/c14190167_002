7b2bf51bbda8af79302bc05932e0f282514f0d16
mchlkevin <77846139+mchlkevin@users.noreply.github.com>
2022-04-07 15:16:40 +0700

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,384 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+        home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Test 1 - C14190167"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Container(
+        padding: EdgeInsets.all(18),
+        child: Column(children: <Widget>[
+          Row(
+            children: [
+              Text("Popular Courses : ",
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0)),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.calendar_month,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text("Science",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0)),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.coffee,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text("Cooking",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0)),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.format_list_numbered,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text("Math",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0)),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.biotech,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text("Biology",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0)),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.star,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  Text("Design",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0)),
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text("Continue Learning : ",
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0)),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.green[100],
+                width: 100,
+                height: 120,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.calendar_month, color: Colors.black, size: 30),
+                    Text("Science",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold)),
+                    Text("Chapter 4",
+                        style: TextStyle(fontSize: 14.0, color: Colors.black)),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.timer_sharp,
+                          color: Colors.black,
+                          size: 15,
+                        ),
+                        Text("27 Mins")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.green[100],
+                width: 100,
+                height: 120,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      Icons.star,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    Text("Design",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold)),
+                    Text("Chapter 5",
+                        style: TextStyle(fontSize: 14.0, color: Colors.black)),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.timer_sharp,
+                          color: Colors.black,
+                          size: 15,
+                        ),
+                        Text("30 Mins")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.green[100],
+                width: 100,
+                height: 120,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.biotech, color: Colors.black, size: 30),
+                    Text("Biology",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold)),
+                    Text("Chapter 1",
+                        style: TextStyle(fontSize: 14.0, color: Colors.black)),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.timer_sharp,
+                          color: Colors.black,
+                          size: 15,
+                        ),
+                        Text("25 Mins")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.green[100],
+                width: 100,
+                height: 120,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.coffee, color: Colors.black, size: 30),
+                    Text("Cooking",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold)),
+                    Text("Chapter 3",
+                        style: TextStyle(fontSize: 14.0, color: Colors.black)),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.timer_sharp,
+                          color: Colors.black,
+                          size: 15,
+                        ),
+                        Text("18 Mins")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text("Last Seen Courses : ",
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0)),
+            ],
+          ),
+          SizedBox(height: 15),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius:
+                      const BorderRadius.all(const Radius.circular(8)),
+                  color: Colors.purple[100],
+                ),
+                width: 500,
+                height: 60,
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.task,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Basics of Designing",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18.0)),
+                        Text("1 hour, 25 mins",
+                            style: TextStyle(fontSize: 12.0))
+                      ],
+                    ),
+                    Icon(
+                      Icons.play_circle_outline,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius:
+                      const BorderRadius.all(const Radius.circular(8)),
+                  color: Colors.purple[100],
+                ),
+                width: 500,
+                height: 60,
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.book,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Human Respiratory System",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18.0)),
+                        Text("4 hours, 10 mins",
+                            style: TextStyle(fontSize: 12.0))
+                      ],
+                    ),
+                    Icon(
+                      Icons.play_circle_outline,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius:
+                      const BorderRadius.all(const Radius.circular(8)),
+                  color: Colors.purple[100],
+                ),
+                width: 500,
+                height: 60,
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.play_lesson_rounded,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Integration & Differentation",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18.0)),
+                        Text("2 hours, 37 mins",
+                            style: TextStyle(fontSize: 12.0))
+                      ],
+                    ),
+                    Icon(
+                      Icons.play_circle_outline,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: <Widget>[
+                      Icon(
+                        Icons.home,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                      Text("Home",
+                          style: TextStyle(color: Colors.blue, fontSize: 20.0)),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.menu_book, color: Colors.grey),
+                      Text("Explore",
+                          style: TextStyle(color: Colors.grey, fontSize: 17.0)),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.chat, color: Colors.grey),
+                      Text("Chat",
+                          style: TextStyle(color: Colors.grey, fontSize: 17.0)),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
